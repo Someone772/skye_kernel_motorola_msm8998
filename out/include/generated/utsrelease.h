b2a8082c8e7ef7ddcab060ef5cb36b549b689464
Aflaungos <vimavick@gmail.com>
2023-05-07 01:43:48 -0300

    Removed out folder

--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.4.302-lineage-ga280cbd9db9e-dirty"
--- out/include/generated/vdso32-offsets.h
+++
@@ -1,4 +0,0 @@
-#define vdso_offset_compat_rt_sigreturn_arm	0x0950
-#define vdso_offset_compat_rt_sigreturn_thumb	0x0970
-#define vdso_offset_compat_sigreturn_arm	0x0940
-#define vdso_offset_compat_sigreturn_thumb	0x0960
