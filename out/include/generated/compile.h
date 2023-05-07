2b34494291f20da1c33d4aba32f8a3783fd990a7
Aflaungos <vimavick@gmail.com>
2023-05-07 01:41:08 -0300

    Added KernelSU using KProbes

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun May 7 00:26:12 -03 2023"
+#define LINUX_COMPILE_BY "mrsiri"
+#define LINUX_COMPILE_HOST "Erick-PC"
+#define LINUX_COMPILER "Android (8490178, based on r450784d) clang version 14.0.6 (https://android.googlesource.com/toolchain/llvm-project 4c603efb0cca074e9238af8b4106c30add4418f6)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.4.302-lineage-ga280cbd9db9e-dirty"
