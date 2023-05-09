175b13c4ff86108e973cef6a5cc29bc86ad6dd4d
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-09 10:05:33 -0300

    msm: kgsl: Dispatch commands using a master kthread
    Instead of coordinating between a worker when dispatching commands and
    abusing a mutex lock for synchronization, it's faster to keep a single
    kthread dispatching commands whenever needed. This reduces GPU
    processing latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [@0ctobot: Adapted for msm-4.9, this reverts commit:
    2eb74d7 ("msm: kgsl: Defer issue commands to worker thread")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2023, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -260,11 +260,6 @@ struct kgsl_device {
 	struct timer_list idle_timer;
 	struct kgsl_pwrctrl pwrctrl;
 	int open_count;
-
-	/* For GPU inline submission */
-	uint32_t submit_now;
-	spinlock_t submit_lock;
-	bool slumber;
 
 	struct mutex mutex;
 	uint32_t state;
