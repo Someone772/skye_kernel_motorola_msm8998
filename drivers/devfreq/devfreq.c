f3ac0d4e995af792fb17688a9bcaec9f88b992e8
Aflaungos <vimavick@gmail.com>
2023-05-09 23:19:35 -0300

    Revert "PM / devfreq: Boost cpubw on screen on events"
    This reverts commit 46e6abcfe8c2cd6ffd4ddfefc9b9b1891f713fca.

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -43,16 +43,6 @@ static LIST_HEAD(devfreq_governor_list);
 static LIST_HEAD(devfreq_list);
 static DEFINE_MUTEX(devfreq_list_lock);
 
-/* List of devices to boost when the screen is woken */
-static const char *boost_devices[] = {
-	"cpubw",
-	NULL
-};
-
-#define WAKE_BOOST_DURATION_MS (10000)
-static struct delayed_work wake_unboost_work;
-static struct work_struct wake_boost_work;
-
 /**
  * find_device_devfreq() - find devfreq struct using device pointer
  * @dev:	device pointer used to lookup device devfreq.
