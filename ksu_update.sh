684f36d3649a481967e38db9cd3b114978cda593
mrsiri <vimavick@gmail.com>
2023-07-10 13:17:32 -0300

    Update KernelSU

--- ksu_update.sh
+++ ksu_update.sh
@@ -2,6 +2,6 @@
 
 echo pulling main branch of KernelSU
 
-git subtree pull --prefix=drivers/staging/kernelsu https://github.com/tiann/KernelSU.git main --squash
+git submodule update --recursive --remote
 
 echo done
