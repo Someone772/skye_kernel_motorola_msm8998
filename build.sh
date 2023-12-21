4f25c10ae64007d72064c595c1f62adb17cfc98f
Aflaungos <vimavick@gmail.com>
2023-12-21 09:33:13 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-echo "Cleaning Last Build..."
 rm -rf out
 mkdir -p out
-echo "Exporting Parameters..."
+
 export ARCH=arm64
 export CLANG_PATH=${HOME}/linux-x86/clang-r416183b/bin
 export PATH=${CLANG_PATH}:${PATH}
@@ -10,8 +9,7 @@ export CROSS_COMPILE_ARM32=${HOME}/arm-linux-androideabi-4.9/bin/arm-linux-andro
 export KERNEL_LLVM_BIN=${HOME}/linux-x86/clang-r416183b/bin/clang
 export LD_LIBRARY_PATH=${HOME}/linux-x86/clang-r416183b/lib64:$LD_LIBRARY_PATH
 export CLANG_TRIPLE=aarch64-linux-gnu-
-echo "Building Kernel..."
+export KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc"
+
 make -j8 -C $(pwd) O=$(pwd)/out AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip $KERNEL_MAKE_ENV ARCH=arm64 CC=clang lineageos_evert_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip $KERNEL_MAKE_ENV ARCH=arm64 CC=clang
-echo "Done!"
-pause
